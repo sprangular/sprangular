ed3e0f8ad01b9ccc31b478a58802441e03f1078c
Josh Nussbaum <joshnuss@gmail.com>
2014-08-13 15:02:43 -0400

    Removed test-unit

--- test/dummy/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-
-  mount Sprangular::Engine => "/sprangular"
-end
--- test/sprangular_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SprangularTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Sprangular
-  end
-end
