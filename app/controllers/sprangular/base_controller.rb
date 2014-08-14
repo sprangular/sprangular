7bcdee6e280514dd455159aa8716416111a19f1a
Josh Nussbaum <joshnuss@gmail.com>
2014-08-14 14:06:19 -0400

    Added fixes for csrf

--- app/controllers/sprangular/base_controller.rb
+++ app/controllers/sprangular/base_controller.rb
@@ -9,6 +9,10 @@ class Sprangular::BaseController < Spree::BaseController
 
   helper Spree::Api::ApiHelpers
 
+  protect_from_forgery
+
+  after_filter :set_csrf_cookie
+
   def invalid_resource!(resource)
     @resource = resource
     render "sprangular/errors/invalid", status: 422
@@ -23,6 +27,14 @@ class Sprangular::BaseController < Spree::BaseController
   end
 
 protected
+
+  def set_csrf_cookie
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
+
+  def verified_request?
+    super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
+  end
 
   def check_authorization
     @user = current_spree_user
