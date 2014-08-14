cef77f05260f6541b04d9fea8bc3dfdd34a725a3
Josh Nussbaum <joshnuss@gmail.com>
2014-08-14 07:40:37 -0400

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Sprangular::Engine.routes.draw do
-  root to: 'home#index'
+  scope module: 'sprangular' do
+    root to: 'home#index'
 
-  namespace :store do
-    post 'facebook/fetch'
+    namespace :store, defaults: {format: :json} do
+      post 'facebook/fetch'
 
-    resources :products
-    resource :cart do
-      post :add_variant
-      put :update_variant
-      delete :remove_variant
+      resources :products
+      resource :cart do
+        post :add_variant
+        put :update_variant
+        delete :remove_variant
+      end
+      resource :account
+      resources :passwords
+      resources :credit_cards
     end
-    resource :account
-    resources :passwords
-    resources :credit_cards
   end
 end
