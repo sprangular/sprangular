1373bfac7ed93e116dc91414e52837891e4cb83d
Josh Nussbaum <joshnuss@gmail.com>
2014-08-14 18:19:41 -0400

    Added products/show action

--- app/controllers/sprangular/products_controller.rb
+++ app/controllers/sprangular/products_controller.rb
@@ -4,4 +4,10 @@ class Sprangular::ProductsController < Sprangular::BaseController
     @products = Spree::Product.active
   end
 
+  def show
+    @product = Spree::Product.active.where(slug: params[:id]).first!
+
+    render 'spree/api/products/show'
+  end
+
 end
