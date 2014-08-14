85af7df63ffd651642ee4755ab9586afdee9a912
Josh Nussbaum <joshnuss@gmail.com>
2014-08-14 07:45:37 -0400

    Updated store/products controller to use api/products/index template

--- app/controllers/sprangular/store/products_controller.rb
+++ app/controllers/sprangular/store/products_controller.rb
@@ -1,7 +1,8 @@
 class Sprangular::Store::ProductsController < Sprangular::Store::BaseController
 
   def index
-    @products = Spree::Product.all
+    @products = Spree::Product.page(params[:page]).per(params[:per_page])
+    render 'spree/api/products/index'
   end
 
 end
