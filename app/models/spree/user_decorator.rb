7528b2d44a71052931335e05dbd26e938c6180ba
Bryan Mahoney <bryan@goDynamo.com>
2014-08-15 11:28:25 -0400

    Adds support for helper methods on Spree::User

---
+++ app/models/spree/user_decorator.rb
@@ -0,0 +1,32 @@
+Spree.user_class.class_eval do
+
+  has_many :user_authentications, :dependent => :destroy
+
+  def past_bill_addresses
+    past_addresses :bill_address
+  end
+
+  def past_ship_addresses
+    past_addresses :ship_address
+  end
+
+  def completed_orders
+    orders.complete.order('updated_at DESC')
+  end
+
+private
+
+  def past_addresses(address_type)
+    addresses = (past_orders_with_most_recent_first(address_type).map(&address_type) + [send(address_type)]).compact
+    addresses.uniq do |a|
+      [a.lastname.to_s, a.firstname.to_s, a.address1.to_s, a.address2.to_s, a.city.to_s, a.zipcode.to_s, a.phone.to_s,
+        a.state_id.to_s, a.state_name.to_s, a.country_id.to_s]
+    end
+  end
+
+  def past_orders_with_most_recent_first(address_type)
+    completed_orders.includes(address_type => [:state, :country])
+  end
+
+end
+
--- lib/sprangular/engine.rb
+++ lib/sprangular/engine.rb
@@ -4,5 +4,13 @@ module Sprangular
       Rails.application.assets.register_mime_type 'text/html', '.html'
       Rails.application.assets.register_engine '.slim', Slim::Template
     end
+
+    def self.activate
+      Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+    end
+    config.to_prepare &method(:activate).to_proc
+
   end
 end
