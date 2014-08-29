6ec8f80cb1d31884599829672daee804ce2450c6
Josh Nussbaum <joshnuss@gmail.com>
2014-08-29 00:36:48 -0400

    Added initializer for rack rewrite

--- lib/sprangular/engine.rb
+++ lib/sprangular/engine.rb
@@ -5,6 +5,12 @@ module Sprangular
       Rails.application.assets.register_engine '.slim', Slim::Template
     end
 
+    initializer "sprangular.add_middleware" do |app|
+      app.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
+        r301 '/products',  '/#!/products'
+      end
+    end
+
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
