903096e12df845426e3c8d8d45fa0f403b73f7de
Josh Nussbaum <joshnuss@gmail.com>
2014-09-08 11:29:23 -0400

    Fix bug with products/index returning duplicates

--- app/controllers/sprangular/products_controller.rb
+++ app/controllers/sprangular/products_controller.rb
@@ -3,7 +3,7 @@ class Sprangular::ProductsController < Sprangular::BaseController
   def index
     @products = Spree::Product.active.includes(:option_types, :product_properties, variants: [:images, :option_values])
     @products = @products.ransack(params[:q]).result if params[:q]
-    @products = @products.page(params[:page]).per(params[:per_page])
+    @products = @products.distinct.page(params[:page]).per(params[:per_page])
 
     render 'spree/api/products/index'
   end
