fbea5600b3fc15935364979e0148c3f81d8a32b1
Josh Nussbaum <joshnuss@gmail.com>
2014-09-12 12:00:07 -0400

    Added routes to install generator

--- lib/generators/sprangular/install/install_generator.rb
+++ lib/generators/sprangular/install/install_generator.rb
@@ -7,7 +7,15 @@ class Sprangular::InstallGenerator < Rails::Generators::Base
   end
 
   def add_assets
-    append_file 'app/assets/javascripts/application.js', "//= require sprangular"
     inject_into_file 'app/assets/stylesheets/application.css', " *= require sprangular\n", before: /\*\//, verbose: true
+    append_file 'app/assets/javascripts/application.js', <<-eos
+//= require sprangular
+//= require sprangular/extraRoutes
+eos
+
+    copy_file 'extraRoutes.coffee', 'app/assets/javascripts/sprangular/extraRoutes.coffee'
+    copy_file 'about.html.slim', 'app/assets/templates/static/about.html.slim'
+    copy_file 'terms.html.slim', 'app/assets/templates/static/terms.html.slim'
+    copy_file 'privacy.html.slim', 'app/assets/templates/static/privacy.html.slim'
   end
 end
---
+++ lib/generators/sprangular/install/templates/extraRoutes.coffee
@@ -0,0 +1,11 @@
+Sprangular.config ($routeProvider) ->
+
+  $routeProvider
+    .when '/about',
+      templateUrl: 'static/about.html'
+
+    .when '/terms',
+      templateUrl: 'static/terms.html'
+
+    .when '/privacy',
+      templateUrl: 'static/privacy.html'
