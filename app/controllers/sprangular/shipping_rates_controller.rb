af811e27346dd8e46a5bd5c2394ae3bffac3f037
Josh Nussbaum <joshnuss@gmail.com>
2014-10-10 12:57:11 -0400

    Added logic to lookup rate based on country/state

--- app/controllers/sprangular/shipping_rates_controller.rb
+++ app/controllers/sprangular/shipping_rates_controller.rb
@@ -3,10 +3,20 @@ class Sprangular::ShippingRatesController < Sprangular::BaseController
     country_id = params[:country_id]
     state_id = params[:state_id]
 
+    if current_order.use_billing
+      update_address(current_order.bill_address ||= Spree::Address.new, country_id, state_id)
+    else
+      update_address(current_order.ship_address ||= Spree::Address.new, country_id, state_id)
+    end
+
     @shipping_rates = shipping_rates
   end
 
 private
+  def update_address(address, country_id, state_id)
+    current_order.ship_address.country_id = country_id
+    current_order.ship_address.state_id = state_id
+  end
 
   def shipping_rates
     packages = Spree::Stock::Coordinator.new(current_order).packages
