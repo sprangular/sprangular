7a7a6bfced5e6805c7b1510613fe124528dfaa29
Josh Nussbaum <joshnuss@gmail.com>
2014-11-13 13:42:42 -0500

    Updated query includes for product search

--- app/controllers/sprangular/products_controller.rb
+++ app/controllers/sprangular/products_controller.rb
@@ -1,7 +1,7 @@
 class Sprangular::ProductsController < Sprangular::BaseController
 
   def index
-    @products = Spree::Product.active.includes(:option_types, :product_properties, variants: [:images, :option_values, :prices])
+    @products = Spree::Product.active.includes(:option_types, :taxons, master: [:images, :option_values, :prices], product_properties: [:property], variants: [:images, :option_values, :prices])
     @products = @products.ransack(params[:q]).result if params[:q]
     @products = @products.distinct.page(params[:page]).per(params[:per_page])
 
