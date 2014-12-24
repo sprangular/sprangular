bb3e2ebe8cdec766e47de9456f2bb46a700b047a
Josh Nussbaum <joshnuss@gmail.com>
2014-12-24 13:06:36 -0500

    Adds caching for products and taxons. Closes #44

--- app/controllers/sprangular/products_controller.rb
+++ app/controllers/sprangular/products_controller.rb
@@ -4,8 +4,9 @@ class Sprangular::ProductsController < Sprangular::BaseController
     @products = Spree::Product.active.includes(:option_types, :taxons, master: [:images, :option_values, :prices], product_properties: [:property], variants: [:images, :option_values, :prices])
     @products = @products.ransack(params[:q]).result if params[:q]
     @products = @products.distinct.page(params[:page]).per(params[:per_page])
+    @cache_key = [I18n.locale, @current_user_roles.include?('admin'), current_currency, params[:q], params[:per_page], params[:per_page]]
 
-    render 'spree/api/products/index'
+    render 'index'
   end
 
   def show
--- app/views/sprangular/products/index.json.rabl
+++ app/views/sprangular/products/index.json.rabl
@@ -1,5 +1,13 @@
 object false
+object false
+cache @cache_key
+
+node(:count) { @products.count }
+node(:total_count) { @products.total_count }
+node(:current_page) { params[:page] ? params[:page].to_i : 1 }
+node(:per_page) { params[:per_page] || Kaminari.config.default_per_page }
+node(:pages) { @products.num_pages }
 
 child(@products => :products) do
-  extends "/sprangular/spree/products/base"
+  extends "spree/api/products/show"
 end
