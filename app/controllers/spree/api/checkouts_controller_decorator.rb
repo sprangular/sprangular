825890699eda7836951ea79c5489064cc63dd343
Josh Nussbaum <joshnuss@gmail.com>
2015-01-04 02:24:09 -0500

    Refactored checkouts/quick_update to persist previously selected shipping method

--- app/controllers/spree/api/checkouts_controller_decorator.rb
+++ app/controllers/spree/api/checkouts_controller_decorator.rb
@@ -3,12 +3,14 @@ module Sprangular::ApiCheckoutControllerDecorator
     load_order(true)
     authorize! :update, @order, order_token
 
+    @selected_rate = find_selected_rate(@order)
+
     revert_to_cart
 
     if params[:complete]
-      advance_to_complete
+      advance_until(&:complete?)
     else
-      advance_to_payment
+      advance_until(&:payment?)
     end
 
     respond_with(@order, default_template: 'spree/api/orders/show')
@@ -19,22 +21,29 @@ private
     @order.update(state: 'cart') unless @order.cart?
   end
 
-  def advance_to_payment
+  def advance_until
     update_order
-    until @order.payment?
-      @order.next!
-    end
+    advance until yield(@order)
   end
 
-  def advance_to_complete
-    update_order
-    until @order.complete?
-      @order.next!
+  def advance
+    if @order.delivery? && @selected_rate
+      shipment = @order.shipments.first
+      available_rate = shipment.shipping_rates.detect {|rate| rate.shipping_method_id == @selected_rate.shipping_method_id && rate.cost == @selected_rate.cost}
+      shipment.update(selected_shipping_rate_id: available_rate.id)
     end
+    @order.next!
   end
 
   def update_order
     @order.update_from_params(params, permitted_checkout_attributes, request.headers.env)
+  end
+
+  def find_selected_rate(order)
+    return unless attrs = params[:order][:shipments_attributes].try(:first)
+
+    shipment = order.shipments.detect {|shipment| shipment.id == attrs[:id].to_i}
+    shipment.shipping_rates.find(attrs[:selected_shipping_rate_id])
   end
 end
 
