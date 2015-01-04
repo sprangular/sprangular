68bf2f2125e004c6b9e83a513ea5f83f5317201b
Josh Nussbaum <joshnuss@gmail.com>
2015-01-04 02:24:53 -0500

    Updated shipping_rates controller to use shipment's rates

--- app/controllers/sprangular/shipping_rates_controller.rb
+++ app/controllers/sprangular/shipping_rates_controller.rb
@@ -9,13 +9,16 @@ class Sprangular::ShippingRatesController < Sprangular::BaseController
       zipcode = ''
     end
 
-    if current_order.use_billing
-      update_address(current_order.bill_address ||= Spree::Address.new, country_id, state_id, zipcode)
+    @order = current_order
+    @order.use_billing = params[:use_billing]
+
+    if @order.use_billing
+      update_address(@order.bill_address ||= Spree::Address.new, country_id, state_id, zipcode)
     else
-      update_address(current_order.ship_address ||= Spree::Address.new, country_id, state_id, zipcode)
+      update_address(@order.ship_address ||= Spree::Address.new, country_id, state_id, zipcode)
     end
 
-    @shipping_rates = shipping_rates
+    render 'spree/api/orders/show'
   end
 
 private
@@ -35,18 +38,7 @@ private
   def update_address(address, country_id, state_id, zipcode)
     address.country_id = country_id
     address.state_id = state_id
-    address.zipcode = zipcode
-  end
-
-  def shipping_rates
-    packages = Spree::Stock::Coordinator.new(current_order).packages
-
-    grouped = packages.map(&:shipping_rates).flatten.inject({}) do |memo, rate|
-      memo[rate.shipping_method_id] ||= Spree::ShippingRate.new(shipping_method: rate.shipping_method, cost: 0)
-      memo[rate.shipping_method_id].cost += rate.cost
-      memo
-    end
-
-    grouped.values
+    address.zipcode = zipcode if zipcode
+    address.save!
   end
 end
