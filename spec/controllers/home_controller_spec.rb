71b104dfa648cf1b04f8468588efe46167cfe0ad
Josh Nussbaum <joshnuss@gmail.com>
2015-01-06 06:48:28 -0500

    Fixed failing spec for home/index

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
-describe Sprangular::HomeController do
+describe Sprangular::HomeController, type: :controller do
   context "GET index" do
     before do
-      get :index
+      allow(controller).to receive_messages(current_spree_user: nil, current_order: nil)
+
+      get :index, use_route: :sprangular_engine
     end
 
     specify { expect(response).to be_success }
