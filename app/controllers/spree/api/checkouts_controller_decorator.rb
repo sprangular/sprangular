4b037259490f8c1a1088cc6ab3041ede30f50187
Josh Nussbaum <joshnuss@gmail.com>
2015-01-13 16:26:28 -0500

    Extract order update logic to class

--- app/controllers/spree/api/checkouts_controller_decorator.rb
+++ app/controllers/spree/api/checkouts_controller_decorator.rb
@@ -3,47 +3,9 @@ module Sprangular::ApiCheckoutControllerDecorator
     load_order(true)
     authorize! :update, @order, order_token
 
-    @selected_rate = find_selected_rate(@order)
-
-    revert_to_cart
-
-    if params[:complete]
-      advance_until(&:complete?)
-    else
-      advance_until(&:payment?)
-    end
+    Sprangular::OrderUpdater.update(@order, request, params)
 
     respond_with(@order, default_template: 'spree/api/orders/show')
-  end
-
-private
-  def revert_to_cart
-    @order.update(state: 'cart') unless @order.cart?
-  end
-
-  def advance_until
-    update_order
-    advance until yield(@order)
-  end
-
-  def advance
-    if @order.delivery? && @selected_rate
-      shipment = @order.shipments.first
-      available_rate = shipment.shipping_rates.detect {|rate| rate.shipping_method_id == @selected_rate.shipping_method_id && rate.cost == @selected_rate.cost}
-      shipment.update(selected_shipping_rate_id: available_rate.id)
-    end
-    @order.next!
-  end
-
-  def update_order
-    @order.update_from_params(params, permitted_checkout_attributes, request.headers.env)
-  end
-
-  def find_selected_rate(order)
-    return unless attrs = params[:order][:shipments_attributes].try(:first)
-
-    shipment = order.shipments.detect {|shipment| shipment.id == attrs[:id].to_i}
-    shipment.shipping_rates.find(attrs[:selected_shipping_rate_id])
   end
 end
 
---
+++ lib/sprangular/order_updater.rb
@@ -0,0 +1,60 @@
+module Sprangular
+  class OrderUpdater
+    include Spree::Core::ControllerHelpers::StrongParameters
+
+    def self.update(order, request, params)
+      new(order, request, params)
+    end
+
+    def initialize(order, request, params)
+      @order   = order
+      @request = request
+      @params  = params
+
+      run
+    end
+
+  private
+
+    def run
+      @selected_rate = find_selected_rate(@order)
+
+      revert_to_cart
+
+      if @params[:complete]
+        advance_until(&:complete?)
+      else
+        advance_until(&:payment?)
+      end
+    end
+
+    def revert_to_cart
+      @order.update(state: 'cart') unless @order.cart?
+    end
+
+    def advance_until
+      update_order
+      advance until yield(@order)
+    end
+
+    def advance
+      if @order.delivery? && @selected_rate
+        shipment = @order.shipments.first
+        available_rate = shipment.shipping_rates.detect {|rate| rate.shipping_method_id == @selected_rate.shipping_method_id && rate.cost == @selected_rate.cost}
+        shipment.update(selected_shipping_rate_id: available_rate.id)
+      end
+      @order.next!
+    end
+
+    def update_order
+      @order.update_from_params(@params, permitted_checkout_attributes, @request.headers.env)
+    end
+
+    def find_selected_rate(order)
+      return unless attrs = @params[:order][:shipments_attributes].try(:first)
+
+      shipment = order.shipments.detect {|shipment| shipment.id == attrs[:id].to_i}
+      shipment.shipping_rates.find(attrs[:selected_shipping_rate_id])
+    end
+  end
+end
