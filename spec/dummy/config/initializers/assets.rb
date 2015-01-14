1f6eb46802d1278be4348819d57c86ac7090738e
braidn <braden.douglass@gmail.com>
2015-01-13 22:38:11 -0500

    Define Teaspoon Assets
    Teaspoon needs a few assets built for precompilation in the dummy app.
    Add them to the dummy assets initializer.

--- spec/dummy/config/initializers/assets.rb
+++ spec/dummy/config/initializers/assets.rb
@@ -6,3 +6,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( teaspoon.css )
+Rails.application.config.assets.precompile += %w( mocha/1.17.1.js )
+Rails.application.config.assets.precompile += %w( teaspoon-mocha.js  )
