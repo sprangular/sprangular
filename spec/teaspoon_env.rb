074dab8f8373d14beb3f8f7c77444960fba26464
braidn <braden.douglass@gmail.com>
2015-01-13 22:36:50 -0500

    Modify Spec Helper For Dummy App
    Since the dummy app is used to grok the way the gem modifies
    Spree/Rails, let's load it during the test. Redefine RAILS_ROOT and
    require to coincide with dummy.

--- spec/teaspoon_env.rb
+++ spec/teaspoon_env.rb
@@ -1,7 +1,7 @@
 # Set RAILS_ROOT and load the environment if it's not already loaded.
 unless defined?(Rails)
-  ENV["RAILS_ROOT"] = File.expand_path("../../", __FILE__)
-  require File.expand_path("../../config/environment", __FILE__)
+  ENV["RAILS_ROOT"] = File.expand_path("../dummy/", __FILE__)
+  require File.expand_path("../dummy/config/environment", __FILE__)
 end
 
 Teaspoon.configure do |config|
