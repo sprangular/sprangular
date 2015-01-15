e52c64559498ad573e241faddcc0e8c4b4cceefd
Rodrigo Dalcin <rodrigo@godynamo.com>
2015-01-15 11:27:28 -0500

    Gets rid of assorted JS Bootstrap dependencies.

--- app/assets/javascripts/sprangular.js
+++ app/assets/javascripts/sprangular.js
@@ -7,16 +7,12 @@
 //= require jquery
 //= require angular
 //= require angularytics
-//= require bootstrap-sass-official
 //= require validity
 
 //
 // Angular specific
 //
 
-//= require angular-bootstrap
-//= require angular-strap/angular-strap.js
-//= require angular-strap/angular-strap.tpl.js
 //= require angular-resource
 //= require angular-route
 //= require angular-sanitize
--- spec/dummy/app/assets/javascripts/application.js
+++ spec/dummy/app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sass-official
 //= require sprangular
 //= require sprangular/extraRoutes
 //= require_tree .
