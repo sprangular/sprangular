36568953bfe7020f2b02f94de07e9e50217cbb2b
Josh Nussbaum <joshnuss@gmail.com>
2015-01-25 11:33:23 -0500

    Ignore js errors when using poltergeist

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -58,7 +58,7 @@ else
   require 'capybara/poltergeist'
 
   Capybara.register_driver :poltergeist do |app|
-    Capybara::Poltergeist::Driver.new(app, timeout: 120)
+    Capybara::Poltergeist::Driver.new(app, timeout: 120, js_errors: false)
   end
 
   Capybara.javascript_driver = :poltergeist
