644ba8637c0608356a20c6b23bcf7e9d8df2442a
Josh Nussbaum <joshnuss@gmail.com>
2015-01-25 10:42:41 -0500

    Added cart instegration spec

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -8,21 +8,6 @@ describe "Visiting Products", js: true do
     ((first_store = Spree::Store.first) && first_store.name).to_s
   end
 
-  it "should increment counter when product added to cart" do
-    visit sprangular_engine.root_path
-
-    product = page.all(:css, '.product')[4]
-    product.hover
-    product.click_link('a')
-
-    expect(page).to have_content("Ruby on Rails Baseball Jersey")
-    expect(page).to have_content("$19.99")
-
-    page.find(:css, '.add-to-cart').click
-
-    expect(page).to have_css(".cart-link .cart-qty", text: "1")
-  end
-
   describe 'meta tags and title' do
     let(:jersey) { Spree::Product.find_by_name('Ruby on Rails Baseball Jersey') }
     let(:metas) { { meta_description: 'Brand new Ruby on Rails Jersey', meta_keywords: 'ror, jersey, ruby' } }
