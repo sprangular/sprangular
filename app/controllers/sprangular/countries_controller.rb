e08abdfc48c98868cb79a0239700530abfd56628
Daniel Wright <daniel@godynamo.com>
2015-03-11 16:14:50 -0400

    Replace Rabl with AMS for CountriesController
    As a model collection grows in size (especially with nested models),
    RABL becomes hilariously unperformant. ActiveModel::Serializers are
    dramatically faster. As an experiment, I've replaced the RABL rendering
    for the CountriesController with AMS.

--- app/controllers/sprangular/countries_controller.rb
+++ app/controllers/sprangular/countries_controller.rb
@@ -1,7 +1,9 @@
 class Sprangular::CountriesController < Sprangular::BaseController
   def index
-    @countries = Spree::Country
-                  .includes(:states)
-                  .order(:name)
+    @countries = Spree::Country.includes(:states).order(:name)
+
+    render json: @countries,
+      root: false,
+      each_serializer: Sprangular::CountrySerializer
   end
 end
