91501e5be1465a5ba84ed5b3477e8f36b2dd8d0e
Bryan Mahoney <bryan@goDynamo.com>
2015-03-17 23:11:28 -0400

    Allow order_attributes to be appended to by extensions. Adds token attribute

--- app/serializers/sprangular/order_serializer.rb
+++ app/serializers/sprangular/order_serializer.rb
@@ -1,18 +1,17 @@
 module Sprangular
   class OrderSerializer < BaseSerializer
-    attributes :id, :number, :item_total, :total, :ship_total, :state,
-               :adjustment_total, :user_id, :created_at, :updated_at,
-               :completed_at, :payment_total, :shipment_state, :payment_state,
-               :email, :special_instructions, :channel, :included_tax_total,
-               :additional_tax_total, :display_included_tax_total,
-               :display_additional_tax_total, :tax_total, :currency,
-               :display_item_total, :display_total, :display_ship_total,
-               :display_tax_total, :checkout_steps
+    attributes *order_attributes
+    attributes | [:display_total, :display_item_total, :display_ship_total, :display_tax_total, :checkout_steps]
 
     attribute :total_quantity
+    attribute :token
 
     def total_quantity
       object.line_items.sum(:quantity)
+    end
+
+    def token
+      object.guest_token
     end
 
     has_one :bill_address, serializer: Sprangular::AddressSerializer
