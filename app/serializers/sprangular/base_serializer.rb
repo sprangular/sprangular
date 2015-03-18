ac6f05842bf4b5f578a55d620a97564bf66f7382
Bryan Mahoney <bryan@goDynamo.com>
2015-03-17 20:43:38 -0400

    Take advantage of built-in caching with AMS Reference: http://hashrocket.com/blog/posts/caching-activemodel-serializers-on-heroku

--- app/serializers/sprangular/base_serializer.rb
+++ app/serializers/sprangular/base_serializer.rb
@@ -3,5 +3,7 @@ require "active_model_serializers"
 module Sprangular
   class BaseSerializer < ActiveModel::Serializer
     extend Spree::Api::ApiHelpers
+    cached
+    delegate :cache_key, to: :object
   end
 end
