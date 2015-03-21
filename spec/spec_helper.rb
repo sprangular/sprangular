d07c1e39cb212faa0abc0361dfa5d7f262c7690f
Russell Osborne <russosborn@gmail.com>
2015-03-21 10:18:47 -0400

    Allow straight  selenium to be run via env flag "firefox"  should allow testing in both CI and Local to match.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -58,6 +58,13 @@ if ENV['WEBDRIVER'] == 'selenium'
   Capybara.register_driver :selenium do |app|
     Capybara::Selenium::Driver.new(app, browser: :chrome)
   end
+elsif ENV['WEBDRIVER'] == 'firefox'
+  require 'selenium-webdriver'
+  Capybara.default_driver = :selenium
+
+  Capybara.register_driver :selenium do |app|
+    Capybara::Selenium::Driver.new(app, browser: :firefox)
+  end
 else
   require 'capybara/poltergeist'
 
