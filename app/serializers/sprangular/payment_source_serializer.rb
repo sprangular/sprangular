0c5d87d2c4c7028540a6fd1aa2c5aaf5dc948413
Bryan Mahoney <bryan@goDynamo.com>
2015-04-17 04:14:22 -0700

    Makes hound a bit happier

--- app/serializers/sprangular/payment_source_serializer.rb
+++ app/serializers/sprangular/payment_source_serializer.rb
@@ -12,33 +12,32 @@ module Sprangular
     end
 
     def month
-      object.respond_to?(:month) ? object.month : ''
+      object.respond_to?(:month) ? object.month : ""
     end
 
     def year
-      object.respond_to?(:year) ? object.year : ''
+      object.respond_to?(:year) ? object.year : ""
     end
 
     def cc_type
-      object.respond_to?(:cc_type) ? object.cc_type : ''
+      object.respond_to?(:cc_type) ? object.cc_type : ""
     end
 
     def last_digits
-      object.respond_to?(:last_digits) ? object.last_digits : ''
+      object.respond_to?(:last_digits) ? object.last_digits : ""
     end
 
     def name
-      object.respond_to?(:name) ? object.name : ''
+      object.respond_to?(:name) ? object.name : ""
     end
 
     def gateway_payment_profile_id
-      object.respond_to?(:gateway_payment_profile_id) ? object.gateway_payment_profile_id : ''
+      object.respond_to?(:gateway_payment_profile_id) ? object.gateway_payment_profile_id : ""
     end
 
     def gateway_customer_profile_id
-      object.respond_to?(:gateway_customer_profile_id) ? object.gateway_customer_profile_id : ''
+      object.respond_to?(:gateway_customer_profile_id) ? object.gateway_customer_profile_id : ""
     end
 
   end
 end
-
