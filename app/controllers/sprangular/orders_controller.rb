401f61f6aaf90894265cf702ae55396305177370
Josh Nussbaum <joshnuss@gmail.com>
2015-04-23 18:27:10 -0400

    Added orders#index action

--- app/controllers/sprangular/orders_controller.rb
+++ app/controllers/sprangular/orders_controller.rb
@@ -1,6 +1,17 @@
 class Sprangular::OrdersController < Sprangular::BaseController
   before_filter :check_authorization
 
+  def index
+    authorize! :show, @user
+
+    @orders = @user.orders.complete
+
+    render json: @orders,
+           scope: current_spree_user,
+           each_serializer: Sprangular::LiteOrderSerializer,
+           root: false
+  end
+
   def show
     authorize! :show, @user
 
