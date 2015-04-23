f2d3195c101544486af9df398ef0ebb9e118360a
Josh Nussbaum <joshnuss@gmail.com>
2015-04-23 18:01:20 -0400

    Add whitespace to UserSerializer

--- app/serializers/sprangular/user_serializer.rb
+++ app/serializers/sprangular/user_serializer.rb
@@ -3,9 +3,9 @@ module Sprangular
     attributes *(user_attributes | [:bill_address_id, :ship_address_id])
 
     has_many :completed_orders, serializer: Sprangular::LiteOrderSerializer
-    has_many :addresses, serializer: Sprangular::AddressSerializer
-    has_many :payment_sources, serializer: Sprangular::PaymentSourceSerializer
-    has_one :order, serializer: Sprangular::OrderSerializer
+    has_many :addresses,        serializer: Sprangular::AddressSerializer
+    has_many :payment_sources,  serializer: Sprangular::PaymentSourceSerializer
+    has_one  :order,            serializer: Sprangular::OrderSerializer
 
     def completed_orders
       object.orders.complete
