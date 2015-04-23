3e780fe958bf1f99aa044bf0e3e258d53e7c99a0
Josh Nussbaum <joshnuss@gmail.com>
2015-04-23 18:27:32 -0400

    Remove completed_orders from /account response

--- app/serializers/sprangular/user_serializer.rb
+++ app/serializers/sprangular/user_serializer.rb
@@ -2,13 +2,8 @@ module Sprangular
   class UserSerializer < BaseSerializer
     attributes *(user_attributes | [:bill_address_id, :ship_address_id])
 
-    has_many :completed_orders, serializer: Sprangular::LiteOrderSerializer
     has_many :addresses,        serializer: Sprangular::AddressSerializer
     has_many :payment_sources,  serializer: Sprangular::PaymentSourceSerializer
-
-    def completed_orders
-      object.orders.complete
-    end
 
     def payment_sources
       object.credit_cards
